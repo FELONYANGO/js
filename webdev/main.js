fdd951c14607c636f22f98688255316ecd129583
FELONYANGO <86606639+FELONYANGO@users.noreply.github.com>
2022-08-31 17:34:31 +0300

    second commit
    continuous minitasks

--- webdev/main.js
+++ webdev/main.js
@@ -1,3 +1,53 @@
 
+
 let no=['red ','blue','white'];
 console.log(no);
+//working on the comparisons
+
+const result = 20 === '20' ? 'values match':'values do not match';
+console.log(result);
+result;
+
+//working on conditional statements
+let checkingValue;
+if(20==20){
+        checkingValue= 'the values are actually matching';
+
+}else{
+    checkingValue = 'values do not match';
+
+}
+// console.log(checkingValue);
+
+// // //=======challenge two==========
+// // const valueOne  = 0;
+// // valueOne++;
+// // valueOne++;
+// // valueOne++;
+
+// // console.log(valueOne);
+
+// // ========challenge two=======
+// const firstNumber = 20;
+// const secondNumber = '20';
+
+// const answer = firstNumber === secondNumber;
+// console.log(answer);
+
+// //=========challenge three two====
+// const expression1 = 100%5;
+// const expression2 =100/50;
+// const expression3 = expression1<expression2;
+// const expression4 = expression3 && 300+5 === 305
+// const expression5 = !expression4
+// console.log(expression4);
+//======challenge four=====
+
+const myobjct ={
+    pop1: 'this is pop one',
+    pop2 : 20
+
+};
+const myArray = [40,50,2];
+const answerInFour = myobjct.pop2 ===(myArray[0]/myArray[2]);
+console.log(answerInFour);
